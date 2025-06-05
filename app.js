4459fc5676a02279b98271a4d08f64c8e5320da0
rsayyed12 <sayyedraashid12@gmail.com>
2025-06-06 00:46:04 +0530

    first commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require('./db'); // Import MySQL connection
 
 app.use(express.json());
 
-// Get all users from MySQL
+// To Get all users from MySQL
 app.get('/users', (req, res) => {
   db.query('SELECT * FROM users', (err, results) => {
     if (err) {
@@ -17,20 +17,9 @@ app.get('/users', (req, res) => {
   });
 });
 
-// Create a new user in MySQL
-// app.post('/users', (req, res) => {
-//   const { name, email } = req.body;
-//   const sql = 'INSERT INTO users (name, emailid) VALUES (?, ?)';
-//   db.query(sql, [name, emailid], (err, result) => {
-//     if (err) {
-//       console.error(err);
-//       return res.status(500).send('Error creating user');
-//     }
-//     res.status(201).json({ message: 'User created', userId: result.insertId });
-//   });
-// });
 
-// Start the server
+
+// To Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
